18b7812291e0761677ae1df1c71d0ca873a25814
Prisca C <prisca.castanier@gmail.com>
2022-10-04 22:57:24 +0200

    starting project

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{ pokeApi.abilities }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'IndexPage',
+    data() {
+      return {
+        pokeApi: [],
+      }
+    },
+    async fetch() {
+      const axios = require('axios');
+
+      this.pokeApi = axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=2000')
+        .then( resp => console.log(resp.data))
+        .catch( error => console.log(error))
+    }
+  }
+</script>
